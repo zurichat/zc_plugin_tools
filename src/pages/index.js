770ebdf513ab6341a172fe0392257bd60b609d47
alvacoder <idrisadeniji01@gmail.com>
2021-08-31 20:48:15 +0100

    add team hobbes text

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ export default function Home() {
         <h1>Edited by CI/CD</h1>
         <h1>Edited Again</h1>
         <h1>Edited  yet Again</h1>
+        <h1>Team Hobbes</h1>
         <h1
           style={{
             marginTop: "20px",
