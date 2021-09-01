f246ddd2d0bf4bbff0ab682063505029aa6c6b56
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-09-01 04:09:29 +0100

    final edit on the app.js file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import Layout from "./components/layout";
-import MainPage from "./components/MainPage/MainPage";
-import SearchBar from "./components/searchbar.component";
-import TestInput from "./components/TextInput/textinput.component";
 
 function App() {
   return (
     <div className="">
-      <Layout>
-        {/* <SearchBar />
-        <MainPage />
-        <TestInput /> */}
-      </Layout>
+      <Layout>{/* Insert your compoents inside this layout entity */}</Layout>
     </div>
   );
 }
