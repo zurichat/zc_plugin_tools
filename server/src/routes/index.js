f77da3ec7b340ca32ece0d768ef5d86a4d5ac391
davien21 <chidiebereekennia@gmail.com>
2021-09-02 16:46:21 +0100

    Feat: Add sidebar route

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const pluginInfoRouter = require("./plugin-info");
+const sideBarRouter = require("./sidebar");
+
 
 module.exports = () => {
   router.use(pluginInfoRouter());
+  router.use(sideBarRouter());
 
   return router;
 };
