61b04f3a8f266429db9395da38d749e014bcd22a
Preshy-Jones <adedibuprecious@gmail.com>
2021-09-02 05:00:44 +0100

    setup figma app authentification

--- client/src/components/Apps/Figma/Figma.js
+++ client/src/components/Apps/Figma/Figma.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Figma = () => {
-  return <div>this is the figma page</div>;
+	const localHostUrl =
+		"https://www.figma.com/oauth?client_id=V2IvPlEApDfBT9uNS5Rbrf&redirect_uri=http://localhost:3000/authenticate/&scope=file_read&state=state&response_type=code";
+	const deployedUrl =
+		"https://www.figma.com/oauth?client_id=V2IvPlEApDfBT9uNS5Rbrf&redirect_uri=http://externaltools.zuri.chat/authenticate/&scope=file_read&state=state&response_type=code";
+	return (
+		<div>
+			<h2 className="text-lg text-gray-600 font-bold">FIGMA HOME PAGE</h2>
+			<div className="mt-6">
+				<a
+					className="px-4 py-2 bg-gray-700 text-white rounded-sm"
+					href={localHostUrl}>
+					Connect to figma
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Figma;
