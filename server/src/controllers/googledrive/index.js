9d0fe51dccc8b7b6d9910b64a800048523ac3130
Oscar Ikechukwu Ekeyekwu <o.ekeyekwu@gems-consult.com>
2021-09-02 14:29:44 +0100

    [googleDrvieApp]: added base backend controller

---
+++ server/src/controllers/googledrive/index.js
@@ -0,0 +1,12 @@
+const response = require("../utils/response");
+
+class GoogleDriveController {
+  async index(req, res) {
+    
+    res.send(response("Google Drive Api Info returned successfully"));
+  }
+
+   
+}
+
+module.exports = new GoogleDriveController();
