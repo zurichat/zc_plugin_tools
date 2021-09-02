2fdaecba22971948b9819f015ce4827634153749
unknown <isaacraphaelchidi@gmail.com>
2021-09-02 17:49:54 +0100

    initial commit

--- client/src/components/Apps/Figma/Figma.js
+++ client/src/components/Apps/Figma/Figma.js
@@ -1,7 +1,26 @@
 import React from "react";
+import './../../searchbar.component'
+import SearchBar from "./../../searchbar.component"
+import FigmaAbout from "./about-secttion/FigmaAbout";
+import  logo from "./images/fignaLogo.png"
+import './css/Figma.css'
 
 const Figma = () => {
-  return <div>this is the figma page</div>;
+  return <>
+    <SearchBar />
+    <div className ='Start-title'>
+      <div className ="figmalogo-container">
+        <img src ={logo} />
+      </div>
+      <h4 className ="Logo-tag">Figma</h4>
+      <div className= "arrow-down">^</div>
+    </div>
+    <div className='welcome-nav'>
+      <h4>Messages</h4>
+      <h4 className='selected'>About</h4>
+    </div>
+    <FigmaAbout />
+  </>;
 };
 
 export default Figma;
