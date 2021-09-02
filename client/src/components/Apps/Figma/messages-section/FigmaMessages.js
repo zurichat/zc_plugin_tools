4f0a39fd2fd48e40548256d82d6f46822a7cd667
Melford-D <melfordbirakor@gmail.com>
2021-09-02 23:29:31 +0100

    added the about section for figma page

---
+++ client/src/components/Apps/Figma/messages-section/FigmaMessages.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ss1 from './../images/ss1.png'
+import logo from './../images/fignaLogo.png'
+import './../css/messages.css'
+
+const FigmaMessages = () => {
+    return <>
+            <div className="messages-welcome">
+                This is a message section
+            </div>
+
+
+    </>
+}
+
+
+
+export default FigmaMessages
