65ff491c3c4a17bf97d171bde9916c1b5c68df0f
Preshy-Jones <adedibuprecious@gmail.com>
2021-09-02 20:32:59 +0100

    setup figma authenticafication

--- client/src/components/Apps/Figma/Figma.js
+++ client/src/components/Apps/Figma/Figma.js
@@ -5,13 +5,15 @@ const Figma = () => {
 		"https://www.figma.com/oauth?client_id=V2IvPlEApDfBT9uNS5Rbrf&redirect_uri=http://localhost:3000/authenticate/&scope=file_read&state=state&response_type=code";
 	const deployedUrl =
 		"https://www.figma.com/oauth?client_id=V2IvPlEApDfBT9uNS5Rbrf&redirect_uri=http://externaltools.zuri.chat/authenticate/&scope=file_read&state=state&response_type=code";
+	const vercelUrl =
+		"https://www.figma.com/oauth?client_id=V2IvPlEApDfBT9uNS5Rbrf&redirect_uri=https://zc-plugin-tools2.vercel.app/authenticate/&scope=file_read&state=state&response_type=code";
 	return (
 		<div>
 			<h2 className="text-lg text-gray-600 font-bold">FIGMA HOME PAGE</h2>
 			<div className="mt-6">
 				<a
 					className="px-4 py-2 bg-gray-700 text-white rounded-sm"
-					href={deployedUrl}>
+					href={vercelUrl}>
 					Connect to figma
 				</a>
 			</div>
