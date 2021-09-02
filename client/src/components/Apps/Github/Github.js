c19df521556163677be423e4207932beb34be258
unknown <saviourise1@gmail.com>
2021-09-02 22:01:22 +0100

    Add github give permission page to zuri chat

--- client/src/components/Apps/Github/Github.js
+++ client/src/components/Apps/Github/Github.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Githubgivepermission from './containers/Githubgivepermission';
 
 const Github = () => {
-  return <div>This is the github page</div>;
+  return <>
+    <Router>
+    <Switch>
+      <Route exact path='/github/githubgivepermission' component={Githubgivepermission} />
+    </Switch>
+  </Router>
+  </>;
 };
 
 export default Github;
