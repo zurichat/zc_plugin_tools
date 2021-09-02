a1b513ab37ffdd8141632d4926539e69eece414e
unknown <isaacraphaelchidi@gmail.com>
2021-09-02 21:40:03 +0100

    adding the figma header section and navigation

--- client/src/components/Apps/Figma/Figma.js
+++ client/src/components/Apps/Figma/Figma.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { Component } from 'react';
 import './../../searchbar.component'
 import SearchBar from "./../../searchbar.component"
 import FigmaAbout from "./about-secttion/FigmaAbout";
+import FigmaMessages from "./messages-section/FigmaMessages";
 import  logo from "./images/fignaLogo.png"
 import './css/Figma.css'
 
-const Figma = () => {
-  return <>
+
+const initialState = {
+  page : 'about'
+}
+class Figma extends Component{
+  constructor() {
+    super()
+    this.state = initialState
+  }
+
+  
+
+  render (){
+    return <>
     <SearchBar />
     <div className ='Start-title'>
       <div className ="figmalogo-container">
@@ -16,11 +29,17 @@ const Figma = () => {
       <div className= "arrow-down">^</div>
     </div>
     <div className='welcome-nav'>
-      <h4>Messages</h4>
-      <h4 className='selected'>About</h4>
-    </div>
-    <FigmaAbout />
+      <h4 onClick ={() => {this.setState({page : 'messages'})}} className={this.state.page === 'about'? '' : 'selected'}>Messages</h4>
+      <h4 onClick ={() => {this.setState({page : 'about'}) }} className={this.state.page === 'about'? 'selected' : ''}>About</h4>
+    </div>{
+      this.state.page === 'about'?
+      <FigmaAbout /> :
+      <FigmaMessages />
+    }
+    
   </>;
-};
+  }
+}
+
 
 export default Figma;
---
+++ client/src/components/Apps/Figma/messages-section/FigmaMessages.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ss1 from './../images/ss1.png'
+import logo from './../images/fignaLogo.png'
+import './../css/messages.css'
+
+const FigmaMessages = () => {
+    return <>
+            <p>This is the message section</p>
+    </>
+}
+
+
+
+export default FigmaMessages
