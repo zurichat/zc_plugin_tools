2daba280c8d540c55be0f84382be3c0ad79615e2
PabloWolf96 <ikhena.owen47@gmail.com>
2021-09-02 21:26:23 +0100

    changed appinfo to apps

--- server/src/controllers/plugin-info.js
+++ server/src/controllers/plugin-info.js
@@ -1,13 +1,13 @@
 const response = require("../utils/response");
 
-const appInfo = ["GITHUB", "FIGMA", "GOOGLE DRIVE", "GIPHY", "GMAIL"];
+const apps = ["GITHUB", "FIGMA", "GOOGLE DRIVE", "GIPHY", "GMAIL"];
 
 class PluginInfoController {
   async getInfo(req, res) {
     res.send(response("Plugin Info returned successfully"));
   }
   async getAllApps(req, res) {
-    res.json(appInfo);
+    res.json(apps);
   }
 }
 
