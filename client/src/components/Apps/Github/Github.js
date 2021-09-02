aa06122e86159c44f4fe8d4db23705cbdfeacf93
Khallekan <okeolalekanisaac@gmail.com>
2021-09-02 22:16:35 +0100

    Update: Fix Issue #287 and #288

--- client/src/components/Apps/Github/Github.js
+++ client/src/components/Apps/Github/Github.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import GithubHome from './containers/GithubHome';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GithubHome from "./containers/GithubHome";
 
 const Github = () => {
   return (
     <>
-      {/* Placeholder searchbar start */}
-      <div>this part comes from zc main</div>
-      {/* Place holder search bar stop */}
       <Router>
         <Switch>
           <Route exact path='/github' component={GithubHome} />
