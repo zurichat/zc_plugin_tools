31a7d283a51591c7c25b12160048677d00d35b8c
Chidiebere Ekennia <54110155+Davien21@users.noreply.github.com>
2021-09-02 11:45:35 +0100

    Revert "Fix Issue #287 and #288"

--- client/src/components/Apps/Github/Github.js
+++ client/src/components/Apps/Github/Github.js
@@ -1,20 +1,7 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import GithubHome from './containers/GithubHome';
+import React from "react";
 
 const Github = () => {
-  return (
-    <>
-      {/* Placeholder searchbar start */}
-      <div>this part comes from zc main</div>
-      {/* Place holder search bar stop */}
-      <Router>
-        <Switch>
-          <Route exact path='/github' component={GithubHome} />
-        </Switch>
-      </Router>
-    </>
-  );
+  return <div>This is the github page</div>;
 };
 
 export default Github;
