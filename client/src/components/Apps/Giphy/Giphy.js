dbfda3cb0ebdefe238e962e1941af87bedd5d9f5
rinsolao <olimeed@gmail.com>
2021-09-03 10:24:36 +0100

    Worked on giphy search bar

--- client/src/components/Apps/Giphy/Giphy.js
+++ client/src/components/Apps/Giphy/Giphy.js
@@ -1,11 +1,13 @@
 import DescriptionAction from "./descriptionActions/descriptionAction";
+import GiphyHeader from "./GiphyHeader/GiphyHeader";
 
 const Giphy = () => {
   return (
     <div>
+      <GiphyHeader />
       <DescriptionAction />
     </div>
-    );
+  );
 };
 
 export default Giphy;
