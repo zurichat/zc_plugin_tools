530b3d5004113af248ff9ee02c823a0468ecccc6
Okpainmo <okpainmoandrew@gmail.com>
2021-09-03 23:43:12 +0100

    reverted a commit to git ignore some set up files

--- client/src/components/Apps/Giphy/Giphy.js
+++ client/src/components/Apps/Giphy/Giphy.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 import MainGiphyLayout from './main-giphy-layout/main-layout';
 // import ToolsHeader from "../../toolsheader/toolsheader";
@@ -16,3 +17,12 @@ const Giphy = () => {
 export default Giphy;
 
 // Please add all your components into the main-layout.js file inside the main-giphy-layout folder not in here.
+=======
+import React from "react";
+
+const Giphy = () => {
+  return <div>This is the giphy page</div>;
+};
+
+export default Giphy;
+>>>>>>> parent of ce0c5cd... Giphy app - I created the main layout for the giphy app where other components will be added
