9dfe14a5eb6370d4c2fe5e71b74be5d161927794
cipherB <bolulawal5@gmail.com>
2021-09-03 10:02:24 +0100

    Update: fixed issue #338

--- client/src/components/Apps/Github/Github.js
+++ client/src/components/Apps/Github/Github.js
@@ -1,7 +1,25 @@
+// import React from "react";
+
+// const Github = () => {
+//   return <div>This is the github page</div>;
+// };
+
+// export default Github;
+
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GithubInstall from "./containers/GithubInstall";
 
 const Github = () => {
-  return <div>This is the github page</div>;
+  return (
+    <>
+      <Router>
+        <Switch>
+          <Route exact path='github/github-installed' component={GithubInstall} />
+        </Switch>
+      </Router>
+    </>
+  );
 };
 
 export default Github;
