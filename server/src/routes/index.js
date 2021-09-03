e74445e81ebe7cb01f3f9c84f7f334377c368331
PabloWolf96 <ikhena.owen47@gmail.com>
2021-09-03 17:50:54 +0100

    get github user

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,12 +1,14 @@
 const router = require("express").Router();
 const pluginInfoRouter = require("./plugin-info");
 const sideBarRouter = require("./sidebar");
+const githubRouter = require("./github");
 const giphy = require("./giphy");
 
 module.exports = () => {
   router.use(pluginInfoRouter());
   router.use(sideBarRouter());
   router.use(giphy());
+  router.use(githubRouter());
 
   // Handle Invalid API routes
   router.use((req, res, next) => {
