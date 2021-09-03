6f6ddb0d054c51bf71a26df82f1864289c85cdf0
Melford-D <melfordbirakor@gmail.com>
2021-09-03 08:09:16 +0100

    added the figma about section

--- client/src/components/Apps/Figma/Figma.js
+++ client/src/components/Apps/Figma/Figma.js
@@ -41,38 +41,4 @@ class Figma extends Component{
   }
 }
 
-
-const initialState = {
-  page : 'about'
-}
-class Figma extends Component{
-  constructor() {
-    super()
-    this.state = initialState
-  }
-
-  render (){
-    return <>
-    <SearchBar />
-    <div className ='Start-title'>
-      <div className ="figmalogo-container">
-        <img src ={logo} />
-      </div>
-      <h4 className ="Logo-tag">Figma</h4>
-      <div className= "arrow-down">^</div>
-    </div>
-    <div className='welcome-nav'>
-      <h4 onClick ={() => {this.setState({page : 'messages'})}} className={this.state.page === 'about'? '' : 'selected'}>Messages</h4>
-      <h4 onClick ={() => {this.setState({page : 'about'}) }} className={this.state.page === 'about'? 'selected' : ''}>About</h4>
-    </div>{
-      this.state.page === 'about'?
-      <FigmaAbout /> :
-      <FigmaMessages />
-    }
-    
-  </>;
-  }
-}
-
-
 export default Figma;
--- client/src/components/Apps/Figma/messages-section/FigmaMessages.js
+++ client/src/components/Apps/Figma/messages-section/FigmaMessages.js
@@ -5,15 +5,7 @@ import './../css/messages.css'
 
 const FigmaMessages = () => {
     return <>
-<<<<<<< HEAD
-            <div className="messages-welcome">
-                This is a message section
-            </div>
-
-
-=======
             <p>This is the message section .</p>
->>>>>>> e6dc97541cff1813251722175ff74204724f1916
     </>
 }
 
