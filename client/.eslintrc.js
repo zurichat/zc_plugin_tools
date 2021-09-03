d5d3ca1ffeac4ea7789ab6f3e66b81f024043b6d
Ekeyekwu Oscar Ikechukwu <oscarekeyekwu@gmail.com>
2021-09-03 07:10:24 +0100

    removed eslint config for frontend

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,22 +1,13 @@
-module.exports = {
-  env: {
-    es2020: true,
-    node: true,
-    jest: true,
-  },
-  extends: ["eslint:recommended", "plugin:prettier/recommended"],
-  parser: "babel-eslint",
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  rules: {
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
-    "no-unused-vars": "off",
-  },
-};
+// module.exports = {
+//   env: {
+//     es2020: true,
+//     node: true,
+//     jest: true,
+//   },
+//   extends: ["eslint:recommended"],
+//   parser: "babel-eslint",
+//   parserOptions: {
+//     ecmaVersion: 11,
+//     sourceType: "module",
+//   }
+// };
