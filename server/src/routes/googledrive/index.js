e83ac471711db52fe8287d024b9b716b469348cb
Olamide Idowu <lammy.idowu@gmail.com>
2021-09-03 19:19:19 +0100

    Feature: Get List Of Files

--- server/src/routes/googledrive/index.js
+++ server/src/routes/googledrive/index.js
@@ -4,6 +4,6 @@ const googleDrvieController = require("../../controllers/googledrive/index");
 
 module.exports = function () {
   router.get("/googledrive", googleDrvieController.index);
-
+  router.get("/googledrive/getFilesList", googleDrvieController.getFilesList);
   return router;
 };
