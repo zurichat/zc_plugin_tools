3c2893e95e7d78a8df263555872a3532574fa43e
Ukaoha Chizoba <ukaohachizoba6@gmail.com>
2021-09-03 11:38:05 +0100

    modified

--- client/src/components/searchbartools.js
+++ client/src/components/searchbartools.js
@@ -11,19 +11,22 @@ import {tools} from "./../data/tools.data";
           d="M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z" />
       </svg>
     </button>
-    <div class="grid grid-cols-4 gap-4" id="tools-list">
+    <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-1"  id="tools-list">
       {
         this.filtered.map(val => (
-          <div>
-            <img src={val.image} alt={val.name+' image'}/>
+          <div class='border-2'>
+            <img width="30%" src={val.image} alt={val.name+' image'}/>
             <p>{val.name}</p>
           </div>
         ))
+
+
       }
     </div>
 
   </div>
   }
+
 
   filtered = [];
 
@@ -36,5 +39,6 @@ import {tools} from "./../data/tools.data";
       })
     }
   }
+
 };
 export default SearchBarTools;
