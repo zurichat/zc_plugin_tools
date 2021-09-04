629e88a7d4aa202f95621e610de3ce91d92b45c6
Longs Pemun Gotar <developerlongs@gmail.com>
2021-09-04 20:43:14 +0100

    added a changes

--- client/src/components/Apps/Figma/messages-section/FigmaMessages.js
+++ client/src/components/Apps/Figma/messages-section/FigmaMessages.js
@@ -1,14 +1,14 @@
 import React from "react";
-import ss1 from './../images/ss1.png'
-import logo from './../images/fignaLogo.png'
-import './../css/messages.css'
+import ss1 from "./../images/ss1.png";
+import logo from "./../images/fignaLogo.png";
+import "./../css/messages.css";
 
 const FigmaMessages = () => {
-    return <>
-            <p>This is the message section .</p>
+  return (
+    <>
+      <p>This is the message section .</p>
     </>
-}
+  );
+};
 
-
-
-export default FigmaMessages
+export default FigmaMessages;
