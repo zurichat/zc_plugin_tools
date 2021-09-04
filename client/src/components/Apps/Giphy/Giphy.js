ff2eb977c1b811a7c316cbc72af801c139b1feb1
rinsolao <olimeed@gmail.com>
2021-09-04 03:09:53 +0100

    Merge conflicts

--- client/src/components/Apps/Giphy/Giphy.js
+++ client/src/components/Apps/Giphy/Giphy.js
@@ -1,11 +1,12 @@
 // import ToolsHeader from "../../toolsheader/toolsheader";
 import DescriptionAction from "./descriptionActions/descriptionAction";
-
+import GiphyHeader from "./GiphyHeader/GiphyHeader";
 const Giphy = () => {
   return (
     <>
       {/* <ToolsHeader /> */}
       <div>
+        <GiphyHeader />
         <DescriptionAction />
       </div>
     </>
