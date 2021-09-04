3865fcbb67775dcafe70909c66ef335b3703f1db
rinsolao <olimeed@gmail.com>
2021-09-04 13:43:15 +0100

    Merge conflicts

--- client/src/components/Apps/Giphy/Giphy.js
+++ client/src/components/Apps/Giphy/Giphy.js
@@ -1,24 +1,17 @@
 // import ToolsHeader from "../../toolsheader/toolsheader";
-<<<<<<< HEAD
 import DescriptionAction from "./descriptionActions/descriptionAction";
 import GiphyHeader from "./GiphyHeader/GiphyHeader";
-=======
-import React from 'react';
-import MainGiphyLayout from './main-giphy-layout/MainGiphyLayout';
+import React from "react";
+import MainGiphyLayout from "./main-giphy-layout/MainGiphyLayout";
 
->>>>>>> 2201f6ed8f1582e961e4f96be557a3233d999249
 const Giphy = () => {
   return (
     <>
       {/* <ToolsHeader /> */}
-<<<<<<< HEAD
       <div>
         <GiphyHeader />
-        <DescriptionAction />
+        <MainGiphyLayout />
       </div>
-=======
-      <MainGiphyLayout />
->>>>>>> 2201f6ed8f1582e961e4f96be557a3233d999249
     </>
   );
 };
