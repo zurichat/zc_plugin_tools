a833a50a3a849181f8bdf8a9a65ea305e265c269
Longs Pemun Gotar <developerlongs@gmail.com>
2021-09-04 21:23:55 +0100

    added message section

--- client/src/components/Apps/Figma/messages-section/FigmaMessages.js
+++ client/src/components/Apps/Figma/messages-section/FigmaMessages.js
@@ -6,7 +6,15 @@ import "./../css/messages.css";
 const FigmaMessages = () => {
   return (
     <>
-      <p>This is the message section .</p>
+      <div className="messages-welcome">
+        <img src={logo}></img>
+        <div className="welcome-info">
+          <h4>This conversation is just between the two of you </h4>
+          <p>
+            Here you can post and recieve comments with <span>@Figma</span>
+          </p>
+        </div>
+      </div>
     </>
   );
 };
