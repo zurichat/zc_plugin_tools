ba6248b047fe63074f4402498483ab07a9a7671c
Richard nwonah <nwonahr@gmail.com>
2021-09-05 12:28:20 +0100

    updated route

--- server/src/routes/figma.js
+++ server/src/routes/figma.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const figmaController = require("../controllers/figma");
 
 module.exports = function () {
-  router.get("/figma/teamproject", figmaController.teamProject);
+  router.get("/figma/teamproject/k3RnvKGOXgCWl0lEtwYP3h", figmaController.teamProject);
 
   return router;
 };
