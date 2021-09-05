820e0c4a607b8427b8b8b859e25170b9a51581ff
Amarachi9 <59516730+Amarachi9@users.noreply.github.com>
2021-09-05 19:39:25 +0100

    updated index.js

--- server/src/routes/googledrive/index.js
+++ server/src/routes/googledrive/index.js
@@ -1,9 +1,22 @@
 const router = require("express").Router();
+const router = require("express").Router();
 
 const googleDrvieController = require("../../controllers/googledrive/index");
 
 module.exports = function () {
   router.get("/googledrive/info", googleDrvieController.index);
   router.get("/googledrive/get-files-list", googleDrvieController.getFilesList);
+  return router;
+};
+
+
+const fetchApiController = require("../controllers/fetch-api");
+
+
+module.exports = function () {
+    console.log()
+  router.get("/googledrive/data", fetchApiController.getApi);
+
+
   return router;
 };
