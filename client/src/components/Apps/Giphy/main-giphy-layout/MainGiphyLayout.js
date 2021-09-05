04be800820c392816d19e2b0a62b93521e64bedf
Oyedolacode <segunzyshegszy12@gmail.com>
2021-09-05 12:09:05 +0100

    Update MainGiphyLayout.js

--- client/src/components/Apps/Giphy/main-giphy-layout/MainGiphyLayout.js
+++ client/src/components/Apps/Giphy/main-giphy-layout/MainGiphyLayout.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './main-layout.css';
 import DescriptionAction from '../descriptionActions/descriptionAction';
+import Description from '../description/Description';
+import Screenshots from '../screenshots/screenshots';
 
 function MainGiphyLayout() {
   return (
     <div className="main-side-section">
-      <DescriptionAction />
+      <div className="top">
+        <DescriptionAction />
+        <Description />
+      </div>
+      <div className="bottom">
+        <Screenshots />
+      </div>
     </div>
   );
 }
