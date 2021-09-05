790b97124e208e6811b812b00ceddef94bd71743
davien21 <chidiebereekennia@gmail.com>
2021-09-05 16:42:30 +0100

    fix env

--- server/src/controllers/giphy.js
+++ server/src/controllers/giphy.js
@@ -1,27 +1,25 @@
-const fetch = require('got')
-const response = require('../utils/response')
-const env = require('../config/env')
+const fetch = require("got");
+const response = require("../utils/response");
+const env = require("../config/env");
 
-const { GIPHY_API_KEY } = env
+const { GIPHY_API_KEY } = env;
 
 const randomGif = async (req, res) => {
-  console.log('g')
-
   const r = await fetch(
-    `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}`,
-  )
-  const { data } = await JSON.parse(r.body)
+    `https://api.giphy.com/v1/gifs/random?api_key=${GIPHY_API_KEY}`
+  );
+  const { data } = await JSON.parse(r.body);
 
-  res.send(response('Random GIF', data))
-}
+  res.send(response("Random GIF", data));
+};
 
 const trendingGifs = async (req, res) => {
   const r = await fetch(
-    `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`,
-  )
-  const { data } = await JSON.parse(r.body)
+    `https://api.giphy.com/v1/gifs/trending?api_key=${GIPHY_API_KEY}`
+  );
+  const { data } = await JSON.parse(r.body);
 
-  res.send(response('Trending GIFs', data))
-}
+  res.send(response("Trending GIFs", data));
+};
 
-module.exports = { randomGif, trendingGifs }
+module.exports = { randomGif, trendingGifs };
