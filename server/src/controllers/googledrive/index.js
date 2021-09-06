eb2c213263728cc171d44fb7254084573d9b88dc
Amarachi9 <59516730+Amarachi9@users.noreply.github.com>
2021-09-06 10:47:08 +0100

    Update index.js

--- server/src/controllers/googledrive/index.js
+++ server/src/controllers/googledrive/index.js
@@ -40,6 +40,12 @@ class GoogleDriveController {
       });
     }
   }
+  
+  async getData(req, res) {
+    const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const json = await resp.json();
+    res.send(response("Data info returned successfully", json));
+  }
 }
 
 module.exports = new GoogleDriveController();
