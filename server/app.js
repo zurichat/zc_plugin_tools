4bc6aaf9b7bcdd67869aad61e718cfd80ce95123
alvacoder <idrisadeniji01@gmail.com>
2021-09-09 14:34:52 +0100

    switch dev db to atlas

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ const logger = require("./src/config/logger");
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger.config));
 
-// require("./src/config/db")();
+require("./src/config/db")();
 require("./src/config/routing")(app);
 
 const PORT = env.PORT;
