75f5dba9c860f14289a5b6c13413a1502bc649d0
Khallekan <okeolalekanisaac@gmail.com>
2021-09-10 18:51:35 +0100

    Remove bullet on github home page, update gmail file structure

--- client/src/components/Apps/Github/containers/GithubHome.js
+++ client/src/components/Apps/Github/containers/GithubHome.js
@@ -1,27 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Image from "../assets/ZV_64LdGoao.jpg";
 import GithubLogo from "../assets/Rectangle 693.jpg";
 import "../style/GithubHome.css";
 const GithubHome = () => {
   return (
-    <section className="github_home github_home_padding">
-      <div className={`github_home_main`}>
+    <section className='github_home github_home_padding'>
+      <div className={`github_home_main min-h-screen`}>
         <section className={`github_home_main-section1`}>
           <div className={`flex flex-col gap-4`}>
             <div
               className={`bg-white flex flex-row items-center justify-center py-6 rounded`}
             >
-              <img src={GithubLogo} alt="github logo" />
+              <img src={GithubLogo} alt='github logo' />
             </div>
             <div className={`github_btn_container flex flex-col gap-2`}>
-              <button
-                onClick={()=> window.open("./github/githubgivepermission")}
+              <Link
+                to={"/github/githubgivepermission"}
+                target='_blank'
+                rel='noopener noreferrer'
                 className={`github_btn_container-btn bg-githubGreen text-white`}
               >
                 Add to Zuri Chat
-              </button>
+              </Link>
               <button
-                disabled="disabled"
+                disabled='disabled'
                 className={`github_btn_container-btn bg-transparent text-githubGreen`}
               >
                 Learn More
@@ -42,16 +45,16 @@ const GithubHome = () => {
                 Currently Free
               </span>
             </p>
-            <p className={`flex flex-col gap-1`}>
+            <div className={`flex flex-col gap-1`}>
               <span className={`font-bold github_home-desc`}>Categories</span>
               <ul
-                className={`font-normal text-lg lg:text-base xl:text-lg 2xl:text-xl text-githubTextGrey flex flex-col gap-1`}
+                className={`font-normal text-lg lg:text-base xl:text-lg 2xl:text-xl text-githubTextGrey flex flex-col gap-1 list-none`}
               >
                 <li className={`github_categories`}>Productivity</li>
                 <li className={`github_categories`}>Communication</li>
                 <li className={`github_categories`}>Developer Tools</li>
               </ul>
-            </p>
+            </div>
           </div>
         </section>
         <section className={`github_home_main-section2`}>
@@ -59,25 +62,25 @@ const GithubHome = () => {
           <div className={`flex flex-col gap-4`}>
             <nav className={`github_home_nav`}>
               <ul
-                className={`flex flex-row items-center gap-4 md:gap-6 lg:gap-12`}
+                className={`w-full max-w-full flex flex-row items-center gap-12 md:gap-6 lg:gap-12 overflow-x-auto`}
               >
                 <li className={`github_home_nav-active github_home_nav-link`}>
                   Description
                 </li>
                 <li className={`github_home_nav-link`}>Features</li>
                 <li className={`github_home_nav-link`}>Permissions</li>
-                <li className={`github_home_nav-link`}>
+                <li className={`github_home_nav-link flex flex-row gap-1`}>
                   Security & complaince
                 </li>
               </ul>
             </nav>
             <div className={`flex flex-col gap-8`}>
-              <section className="bg-githubBlue flex flex-row gap-1 lg:gap-0 items-center lg  text-white font-bold p-5 rounded">
-                <h2 className="w-6/12 lg:w-2/6 text-xl lg:text-2xl 2xl:text-4xl ">
+              <section className='bg-githubBlue flex flex-row gap-1 lg:gap-0 items-center lg text-white font-bold p-4 md:p-5 lg:p-5 rounded'>
+                <h2 className='w-6/12 lg:w-2/6 md:text-3xl lg:text-2xl 2xl:text-4xl '>
                   Bring your code to the conversations you care about
                 </h2>
-                <div className="w-6/12 lg:w-4/6 rounded overflow-hidden">
-                  <img src={Image} alt="repository" className="w-full" />
+                <div className='w-6/12 lg:w-4/6 rounded overflow-hidden'>
+                  <img src={Image} alt='repository' className='w-full' />
                 </div>
               </section>
               <div
@@ -87,15 +90,15 @@ const GithubHome = () => {
                   Stay up to date about what’s happening on github without
                   leaving Zuri by connecting GitHub and Zuri app.
                 </p>
-                <p className={`flex flex-col gap-2`}>
+                <div className={`flex flex-col gap-2`}>
                   <span>With Github on Zuri, you can stay updated on:</span>
-                  <ul className="list-disc flex flex-col gap-1.5 list-inside">
+                  <ul className='list-disc flex flex-col gap-1.5 list-inside'>
                     <li>New commits </li>
                     <li>New pull requests</li>
                     <li> New commits</li>
                     <li> Code reviews</li>
                   </ul>
-                </p>
+                </div>
               </div>
             </div>
           </div>
--- client/src/components/Apps/Gmail/Gmail.js
+++ client/src/components/Apps/Gmail/Gmail.js
@@ -3,48 +3,68 @@ import React, { useState } from "react";
 // import { faAngleDoubleLeft } from "@fortawesome/free-solid-svg-icons";
 // import { useHistory } from "react-router-dom";
 // import ToolsHeader from "../../toolsheader/toolsheader";
-import "./Gmail.css";
-import gmailLayout from "../../../assets/gmailLayout.png";
-import gmailLogo from "../../../assets/gmailLogo.png";
+import "./style/Gmail.css";
+import gmailLayout from "./assets/gmailLayout.png";
+import gmailLogo from "./assets/gmailLogo.png";
 
 const Gmail = () => {
   const [clicked, setClicked] = useState("desc");
+
   return (
-    <div className="container mt-9">
-      <div className="container-left flex flex-col">
-        <div className="flex flex-col self-baseline mb-3">
-          <img src={gmailLogo} alt="Gmail Logo" className="w-60" />
-          <div className="flex flex-col mt-4">
-            <button className="btn text-white bg-green-600 hover:bg-green-500 font-extrabold text-sm p-3">
+    <div className='container mt-9'>
+      <div className='container-left flex flex-col'>
+        <div className='flex flex-col self-baseline mb-3'>
+          <img src={gmailLogo} alt='Gmail Logo' className='w-60' />
+          <div className='flex flex-col mt-4'>
+            <button className='btn text-white bg-green-600 hover:bg-green-500 font-extrabold text-sm p-3'>
               Add to Zuri
             </button>
-            <button className="btn mt-2 border-green-600 text-green-600 hover:bg-green-600 hover:text-white font-extrabold text-sm p-3">
+            <button className='btn mt-2 border-green-600 text-green-600 hover:bg-green-600 hover:text-white font-extrabold text-sm p-3'>
               Learn More
             </button>
           </div>
-          <div className="mt-7 h-px bg-gray-400"></div>
+          <div className='mt-7 h-px bg-gray-400'></div>
         </div>
 
-        <p className="font-bold">Supported Languages</p>
-        <p className="mb-4 text-sm">English</p>
-        <p className="font-bold">Pricing</p>
-        <p className="mb-4 text-sm">Free</p>
-        <p className="font-bold">Categories</p>
-        <div className="pt-3">
-          <button className="p-6 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold">
+        <div className={`hidden lg:flex flex-col gap-3`}>
+          <p className={`flex flex-col gap-1`}>
+            <span className={`font-bold gmail_home-desc`}>
+              Supported Languages
+            </span>
+            <span className={`font-normal gmail_home-desc`}>English</span>
+          </p>
+          <p className={`flex flex-col gap-1`}>
+            <span className={`font-bold gmail_home-desc`}>Pricing</span>
+            <span className={`font-normal gmail_home-desc`}>
+              Currently Free
+            </span>
+          </p>
+          <div className={`flex flex-col gap-1`}>
+            <span className={`font-bold gmail_home-desc`}>Categories</span>
+            <ul
+              className={`font-normal text-lg lg:text-base xl:text-lg 2xl:text-xl text-gmailTextGrey flex flex-col gap-1`}
+            >
+              <li className={`gmail_categories`}>Productivity</li>
+              <li className={`gmail_categories`}>Communication</li>
+              <li className={`gmail_categories`}>Developer Tools</li>
+            </ul>
+          </div>
+        </div>
+        {/* <div className='pt-3'>
+          <button className='p-6 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold'>
             Productivity
           </button>
-          <button className="p-4 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold">
+          <button className='p-4 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold'>
             Project Management
           </button>
-          <button className="p-4 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold">
+          <button className='p-4 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold'>
             Other Tools
           </button>
-        </div>
+        </div> */}
       </div>
-      <div className="container-right">
-        <h1 className="text-4xl font-black mb-5">Gmail</h1>
-        <div className=" border-b w-full flex mb-4">
+      <div className='container-right'>
+        <h1 className='text-4xl font-black mb-5'>Gmail</h1>
+        <div className=' border-b w-full flex mb-4'>
           <button
             onClick={() => setClicked("desc")}
             className={`mr-4  border-b  px-2 hover:text-green-600 ${
@@ -86,71 +106,71 @@ const Gmail = () => {
           </button>
         </div>
         {clicked === "desc" ? (
-          <div className=" w-full rounded-md pt-2">
-            <img src={gmailLayout} alt="Gmail Layout illustration" />
+          <div className=' w-full rounded-md pt-2'>
+            <img src={gmailLayout} alt='Gmail Layout illustration' />
           </div>
         ) : null}
         {clicked === "desc" ? (
-          <div className="mt-4">
-            <p className="">
+          <div className='mt-4'>
+            <p className=''>
               Keep conversations moving forward by bringing context from an
               email into a Zuri channel or direct message. You can choose to add
               a message and include attachments along with the email as well.
             </p>
-            <h3 className="pt-3 font-bold">When to send email into Zuri:</h3>
-            <ul className="list-inside list-disc">
-              <li className="">
+            <h3 className='pt-3 font-bold'>When to send email into Zuri:</h3>
+            <ul className='list-inside list-disc'>
+              <li className=''>
                 Email chain getting too noisy? Send it into Zuri Chat where you
                 can discuss quickly and move forward.
               </li>
-              <li className="">
+              <li className=''>
                 Have an idea you want to share with your team? Send it along to
                 a relevant channel with a note about your insights.
               </li>
-              <li className="">
+              <li className=''>
                 Flag your teammates in Zuri Chat and use the email content to
                 set context.
               </li>
             </ul>
 
-            <p className="pt-4 font-bold">How the Add-On works:</p>
-            <ul className="list-inside list-disc">
-              <li className="py-1">
+            <p className='pt-4 font-bold'>How the Add-On works:</p>
+            <ul className='list-inside list-disc'>
+              <li className='py-1'>
                 Start by installing the Zuri Chat for Gmail Add-On from the G
                 Suite Marketplace
               </li>
-              <li className="">
+              <li className=''>
                 Open any email and click on the Zuri icon in the right-hand side
                 of your inbox
               </li>
-              <li className="">
+              <li className=''>
                 Choose the channel or person you’d like to forward the email to
                 inside of Zuri
               </li>
-              <li className="">
+              <li className=''>
                 Add a message and include attachments if you’d like
               </li>
-              <li className="">
+              <li className=''>
                 Voilà! The email is forwarded into Zuri Chat where anyone in the
                 channel or direct message you shared it to can see the email
                 content and open attachments.
               </li>
             </ul>
 
-            <p className="pt-4 font-bold">
+            <p className='pt-4 font-bold'>
               Please note that the following limits apply to emails shared via
               this app:
             </p>
-            <ul className="list-inside list-disc">
-              <li className="py-1">
+            <ul className='list-inside list-disc'>
+              <li className='py-1'>
                 The combined headers and body of an email cannot be larger than
                 1MB.
               </li>
-              <li className="py-1">
+              <li className='py-1'>
                 When combined with attachments, an email cannot be larger than
                 30 MB.
               </li>
-              <li className="py-1">
+              <li className='py-1'>
                 There is a maximum of 20 attachments per email.
               </li>
             </ul>
@@ -158,54 +178,54 @@ const Gmail = () => {
         ) : null}
 
         {clicked === "sec" ? (
-          <div className="mt-4">
-            <div className="flex spa">
-              <div className="bg-white w-full py-4 px-4 flex justify-between rounded items-center h-30">
+          <div className='mt-4'>
+            <div className='flex spa'>
+              <div className='bg-white w-full py-4 px-4 flex justify-between rounded items-center h-30'>
                 <p>Download security and compliance information</p>
-                <button className="btn p-7 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold">
+                <button className='btn p-7 border-1 border-gray-500 py-2 text-gray-800 text-sm my-1 rounded hover:text-black hover:font-extrabold'>
                   Download CSV
                 </button>
               </div>
             </div>
-            <div className="h-px w-full bg-gray-300"></div>
-            <div className="bg-white w-full py-4 px-4 flex justify-between rounded items-center">
+            <div className='h-px w-full bg-gray-300'></div>
+            <div className='bg-white w-full py-4 px-4 flex justify-between rounded items-center'>
               <p>
                 Review the details to better understand this app’s security
                 practices. To learn more about assessing apps for your workspace
                 visit our{" "}
-                <a href="/home" className="text-green-600">
+                <a href='/home' className='text-green-600'>
                   Help Center
                 </a>
-                <div className="link mt-9">
-                  <div className="mt-3">
-                    <h3 className="font-extrabold">General</h3>
+                <div className='link mt-9'>
+                  <div className='mt-3'>
+                    <h3 className='font-extrabold'>General</h3>
                     {/* <i className="fa fa-angle-right"></i> */}
                   </div>
-                  <div className="mt-3 h-px bg-gray-300"></div>
-                  <div className="mt-3">
-                    <h3 className="font-extrabold">
+                  <div className='mt-3 h-px bg-gray-300'></div>
+                  <div className='mt-3'>
+                    <h3 className='font-extrabold'>
                       Privacy & data governance
                     </h3>
                     {/* <i className="fa fa-angle-right"></i> */}
                   </div>
-                  <div className="mt-3 h-px bg-gray-300"></div>
-                  <div className="mt-3">
-                    <h3 className="font-extrabold">
+                  <div className='mt-3 h-px bg-gray-300'></div>
+                  <div className='mt-3'>
+                    <h3 className='font-extrabold'>
                       Certifications & compliance
                     </h3>
                     {/* <i className="fa fa-angle-right"></i> */}
                   </div>
-                  <div className="mt-3 h-px bg-gray-300"></div>
-                  <div className="mt-3">
-                    <h3 className="font-extrabold">Security</h3>
+                  <div className='mt-3 h-px bg-gray-300'></div>
+                  <div className='mt-3'>
+                    <h3 className='font-extrabold'>Security</h3>
                     {/* <i className="fa fa-angle-right"></i> */}
                   </div>
                 </div>
               </p>
             </div>
-            <div className="mt-5"></div>
-            <div className="bg-white w-full py-4 px-4 rounded items-center">
-              <h3 className="font-extrabold mb-3">Scopes</h3>
+            <div className='mt-5'></div>
+            <div className='bg-white w-full py-4 px-4 rounded items-center'>
+              <h3 className='font-extrabold mb-3'>Scopes</h3>
               <p>ZuriChat for Gmail has 8 User Token Scopes</p>
             </div>
           </div>
