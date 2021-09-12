b268e81f262afa6b135d577c4942eb8163b1aaee
Longs Pemun Gotar <developerlongs@gmail.com>
2021-09-12 03:49:26 +0100

    finish building the ui for desktop view

--- client/src/components/Apps/Figma/about-secttion/Screenshots.js
+++ client/src/components/Apps/Figma/about-secttion/Screenshots.js
@@ -1,12 +1,15 @@
 import React from "react";
-
+import ss1 from "./../images/ss1.png";
+import ss2 from "./../images/ss2.png";
+import ss3 from "./../images/ss3.png";
+import styles from "./FigmaAbout.module.css";
 function Screenshots() {
   return (
     <div>
-      <div className="screenshots-heading-container">
+      <div className={styles.screenshots_title}>
         <h4>Screenshots</h4>
       </div>
-      <div className="screenshots-container">
+      <div className={styles.screenshots_container}>
         <img src={ss1}></img>
         <img src={ss2}></img>
         <img src={ss3}></img>
