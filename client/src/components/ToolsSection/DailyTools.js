b59309c58d317d946692951ada4ae142fa17a0cd
Kehinded <ayenikehinded@gmail.com>
2021-09-13 18:46:29 +0100

    tool directory page and search filter functionality

---
+++ client/src/components/ToolsSection/DailyTools.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import Tools from "../fragments/tools/Tools";
+import Tool from "../fragments/tools/Tool";
+
+const DailyTools = ({list, text}) => {
+    return (
+      <div style={{ width: "100%", margin: "1rem 0rem 2rem 0rem" }}>
+        {list.length > 0 ? (
+          <Tools>
+            {list.map((item, index) => (
+              <Tool
+                key={index}
+                icon={item.image}
+                title={item.name}
+                text={item.description}
+                pad={false}
+              />
+            ))}
+          </Tools>
+        ) : (
+          `No result of  "${text}"  found for daily tools`
+        )}
+      </div>
+    );
+}
+
+export default DailyTools
