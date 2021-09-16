fc25fba0367fe1463d826d6245acdef5c6179c4d
davien21 <chidiebereekennia@gmail.com>
2021-09-16 14:57:57 +0100

    test cors fix --2

--- server/src/config/routing.js
+++ server/src/config/routing.js
@@ -19,7 +19,11 @@ module.exports = (app) => {
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(cors({ origin: "*", credentials: true }));
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  });
+  app.use(cors({ origin: "*", credentials: true, allowedHeaders: "*" }));
 
   app.use("/api", routes);
 
