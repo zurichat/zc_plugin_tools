72d26af2bb5988ed404f8c83ed522aa0f8d5bae5
davien21 <chidiebereekennia@gmail.com>
2021-09-16 14:46:45 +0100

    fix cors errors

--- server/src/config/routing.js
+++ server/src/config/routing.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
-  app.use(cors());
+  app.use(cors({ origin: "*", credentials: true }));
 
   app.use("/api", routes);
 
